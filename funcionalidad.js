b20edf11061ab89756fa623176ba7ee3ee2c01c2
JorgeRM <ramosmunozjorgeluis2@gmail.com>
2023-01-19 21:24:30 -0500

    first commit

---
+++ funcionalidad.js
@@ -0,0 +1,10 @@
+let enlaceBrowse = document.getElementById("enlaceBrowse")
+let pestañacamp =document.getElementById("pestaña-camp")
+
+ function toggleAper() {
+    enlaceBrowse.classList.toggle("open")
+ }
+
+ function infoCampana() {
+    pestañacamp.classList.toggle("open")
+ }
